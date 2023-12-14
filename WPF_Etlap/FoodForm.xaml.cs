a1f37ca4258fe74c90a5900370ac41e774921cd9
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-14 10:39:13 +0100

    Creating new data now works

--- WPF_Etlap/FoodForm.xaml.cs
+++ WPF_Etlap/FoodForm.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -28,7 +29,70 @@ namespace WPF_Etlap
 
         private void buttonAddFood_Click(object sender, RoutedEventArgs e)
         {
+            string name = textBoxName.Text.Trim();
+            string description = textBoxDescription.Text.Trim();
+            string category = "";
+            if ((bool)radioButtonAppetizer.IsChecked)
+            {
+                category = "előétel";
+            }
+            else if ((bool)radioButtonMain.IsChecked)
+            {
+                category = "főétel";
+            }
+            else if ((bool)radioButtonDessert.IsChecked)
+            {
+                category = "desszert";
+            }
 
+            string priceString = textBoxPrice.Text.Trim();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Étel nevének megadása kötelező!");
+                return;
+            }
+            if (string.IsNullOrEmpty(description))
+            {
+                MessageBox.Show("Étel leírásának megadása kötelező!");
+                return;
+            }
+            if (!int.TryParse(priceString, out int price))
+            {
+                MessageBox.Show("Az ár csak szám lehet!");
+                return;
+            }
+            if (price < 1)
+            {
+                MessageBox.Show("Az ár csak pozitív szám lehet!");
+                return;
+            }
+            if (!(bool)radioButtonAppetizer.IsChecked && !(bool)radioButtonMain.IsChecked && !(bool)radioButtonDessert.IsChecked)
+            {
+                MessageBox.Show("Kategória megadása kötelező!");
+                return;
+            }
+
+            Food newFood = new Food();
+            newFood.Name = name;
+            newFood.Description = description;
+            newFood.Price = price;
+            newFood.Category = category;
+
+            if (this.service.Create(newFood))
+            {
+                MessageBox.Show("Sikeres hozzáadás!");
+                textBoxName.Text = "";
+                textBoxDescription.Text = "";
+                textBoxPrice.Text = "";
+                radioButtonAppetizer.IsChecked = false;
+                radioButtonMain.IsChecked = false;
+                radioButtonDessert.IsChecked = false;
+            }
+            else
+            {
+                MessageBox.Show("Hiba történt a Hozzáadás során!");
+            }
         }
     }
 }
